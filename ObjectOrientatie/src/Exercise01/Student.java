d92809fd34394da6f35e75c4a066e26ebfc04057
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-02-03 00:25:51 +0100

    Imported the whole project into Github

---
+++ ObjectOrientatie/src/Exercise01/Student.java
@@ -0,0 +1,26 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise01;
+
+/**
+ *
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+public class Student {
+    private String voornaam;
+    private String achternaam;
+    private int studentnr;
+    
+    /*
+     * Default constructor.
+     */
+    public Student (String voornaam, String achternaam, int studentnr) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.studentnr = studentnr;
+    }
+}
