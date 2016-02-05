8649d5039b329f5a21cdfe8f7a997d7eaafd6db1
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-02-05 13:44:18 +0100

    Merged Hoofdklasse and Exercise01 -- Hoofdklasse is now deprecated

--- ObjectOrientatie/src/Exercise01/Exercise01.java
+++ ObjectOrientatie/src/Exercise01/Exercise01.java
@@ -5,6 +5,8 @@
 
 package Exercise01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Joep Veldhoven (s4456556)
@@ -13,6 +15,65 @@ package Exercise01;
 public class Exercise01 {
     
     public static void main (String[] args) {
-        Hoofdklasse hoofdklasse = new Hoofdklasse ();
+        Exercise01 exercise01 = new Exercise01 ();
+    }
+    
+    /**
+     * Default constructor
+     */
+    public Exercise01 () {
+        
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Hoe groot moet de groep studenten worden?");
+        int aantal = scanner.nextInt();
+        scanner.nextLine(); // flush because nextInt is annoying
+        if (aantal > 0) { // The user wants a list of more than 0 elements.
+            Groep g = new Groep (aantal);
+            
+            for (int i = 0; i < aantal; i ++) {
+                System.out.println("Voornaam van student " + String.valueOf(i + 1) + "?");
+                String voornaam = scanner.nextLine();
+                System.out.println("Achternaam van student " + String.valueOf(i + 1) + "?");
+                String achternaam = scanner.nextLine();
+                System.out.println("Studentnr van student " + String.valueOf(i + 1) + "?");
+                int studentnr = scanner.nextInt();
+                scanner.nextLine();
+                
+                if (studentnr < 0) { // If the studentnr is negative, stop the creation of the list
+                    break;
+                }
+                
+                Student s = new Student (voornaam, achternaam, studentnr);
+                g.voegToe(s);
+            }
+            
+            while(true) {
+                System.out.println(g.toString());
+                
+                System.out.println("Volgnummer van student?");
+                int volgnr = scanner.nextInt();
+                scanner.nextLine();
+                if (volgnr < 0) {
+                    break;
+                }
+                if (volgnr >= aantal){
+                    System.out.println("Volgnummer te hoog");
+                    
+                }
+                else{
+                    System.out.println("Voornaam van student?");
+                    String voornaam = scanner.nextLine();
+                    System.out.println("Achternaam van student?");
+                    String achternaam = scanner.nextLine();
+                    Student s = g.getStudent(volgnr);
+                    if (s != null) {
+                        s.setNaam(voornaam, achternaam);
+                        g.setStudent(volgnr, s);
+                    }
+                }
+            }
+        }
+        
     }
 }
