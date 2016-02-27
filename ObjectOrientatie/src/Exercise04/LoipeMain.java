c8a4649f4a106e7414b41cd62ca14c549af95b95
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-02-27 19:37:18 +0100

    Added a view and a main.

---
+++ ObjectOrientatie/src/Exercise04/LoipeMain.java
@@ -0,0 +1,21 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise04;
+
+/**
+ *
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+
+
+public class LoipeMain {
+    
+    public static void main (String[] args) {
+        LoipeView lv = new LoipeView();
+    }
+    
+}
---
+++ ObjectOrientatie/src/Exercise04/LoipeView.java
@@ -0,0 +1,29 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+
+
+public class LoipeView {
+    
+    public LoipeView () {
+        Scanner s = new Scanner (System.in);
+        
+        System.out.println("Welcome to Loipe!");
+        System.out.println("Please enter your input:");
+        String loipeInput = s.nextLine();
+        
+        Loipe l = new Loipe(loipeInput);
+    }
+    
+}
