ed7d691b615d33be31f125791b48c015321b4dbe
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-03-05 12:09:24 +0100

    Added all the necessary (empty) classes.

---
+++ ObjectOrientatie/src/Exercise05/Question.java
@@ -0,0 +1,17 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise05;
+
+/**
+ * 
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+
+
+public class Question {
+    
+}
