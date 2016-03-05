03b013aa8d72ce402c6ced90ca29dbcc7cf13700
veldhojo <jpj.v@hotmail.com>
2016-03-05 16:58:05 +0100

    javadoc MC question & open question

--- ObjectOrientatie/src/Exercise05/MCQuestion.java
+++ ObjectOrientatie/src/Exercise05/MCQuestion.java
@@ -8,18 +8,25 @@ package Exercise05;
 import java.util.Random;
 
 /**
- * 
+ * a multiple choice question
  * @author Joep Veldhoven (s4456556)
  * @author Tijmen van der Kemp (s4446887)
  */
 
 public class MCQuestion extends Question {
     
-    private String question;
-    private String[] answers;
-    private int correct;
-    private int weight;
+    private String question; // the question
+    private String[] answers; // the possible answers
+    private int correct; // the number of the correct answer
+    private int weight; // weight of the question
     
+    /**
+     * constructor for multiple choice question
+     * @param question the question
+     * @param answers the possible answers
+     * @param correct the number of the correct answer
+     * @param weight the weight of the question if between 1 and 5 else default is 3
+     */
     public MCQuestion(String question, String[] answers, int correct, int weight){
         super(question, weight);
         this.answers = answers;
@@ -27,6 +34,12 @@ public class MCQuestion extends Question {
         
     }
     
+    /**
+     * constructor for multiple choice question without specified weight, putting it to the default 3
+     * @param question the question
+     * @param answers the possible answers
+     * @param correct the number of the correct answer
+     */
     public MCQuestion(String question, String[] answers, int correct){
         super(question, 3);
         this.answers = answers;
@@ -34,6 +47,10 @@ public class MCQuestion extends Question {
         
     }
     
+    /**
+     * gives a string to output a multiple choice question
+     * @return a string of the question following all possible answers
+     */
     @Override
     public String toString(){
         String output = question;
@@ -43,16 +60,29 @@ public class MCQuestion extends Question {
         return output;
     }
     
+    /**
+     * determines whether a given answer is correct
+     * @param answer a string of an integer
+     * @return whether the integer given is the same as the correct integer and thus the given answer is correct
+     */
     @Override
     public boolean isCorrect(String answer){
         return Integer.parseInt(answer) == correct;
     }
     
+    /**
+     * gives the correct answer to the question
+     * @return the correct answer
+     */
     @Override
     public String correctAnswer() {
         return answers[correct];
     }
     
+    /**
+     * duplicates a multiple choice question and rotates around the answers
+     * @return the same multiple choice question with the answers rotated and the correct answer changed accordingly
+     */
     @Override
     public MCQuestion duplicate(){
         Random random = new Random();
--- ObjectOrientatie/src/Exercise05/OpenQuestion.java
+++ ObjectOrientatie/src/Exercise05/OpenQuestion.java
@@ -6,42 +6,70 @@
 package Exercise05;
 
 /**
- * 
+ * an open question
  * @author Joep Veldhoven (s4456556)
  * @author Tijmen van der Kemp (s4446887)
  */
 
 public class OpenQuestion extends Question {
-    private String question;
-    private String answer;
-    private int weight;
+    private String question; // the question
+    private String answer; // the answer
+    private int weight; //weight of the question
     
+    /**
+     * constructor for open question
+     * @param question the question
+     * @param answer the answer
+     * @param weight the weight of the question if between 1 and 5 else default is 3
+     */
     public OpenQuestion(String question, String answer, int weight){
         super(question, weight);
         this.answer = answer;
         
     }
     
+     /**
+     * constructor for multiple choice question without specified weight, putting it to the default 3
+     * @param question the question
+     * @param answer the correct answer
+     */
     public OpenQuestion(String question, String answer){
         super(question, 3);
         this.answer = answer;
     }
     
+    /**
+     * a string of an open question
+     * @return the question
+     */
     @Override
     public String toString(){
         return question;
     }
     
+    /**
+     * determines whether the given answer is correct
+     * @param answer the answer to check if correct
+     * @return whether the given answer equals the correct answer.
+     */
     @Override
     public boolean isCorrect(String answer){
         return(answer.equalsIgnoreCase(this.answer));
     }
     
+    /**
+     * gives the correct answer
+     * @return the correct answer
+     */
     @Override
     public String correctAnswer() {
         return answer;
     }
     
+    /**
+     * duplicates the open question
+     * @return the same question
+     */
     @Override
     public OpenQuestion duplicate(){
         return this;
