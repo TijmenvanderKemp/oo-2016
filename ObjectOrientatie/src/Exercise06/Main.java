9f54b9c928466217ed9b0bf177e8c7f7845b30e1
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-03-12 22:09:18 +0100

    Altered the test case to something I know is solvable

--- ObjectOrientatie/src/Exercise06/Main.java
+++ ObjectOrientatie/src/Exercise06/Main.java
@@ -8,8 +8,8 @@ public class Main
 {
 
     public static void main(String[] args) {
-        int [] game = {5,2,7, 1,3,8, 6,9,4}; // Hard test case
-//        int [] game = {1,2,3, 9,5,6, 4,7,8}; // Easy test case
+//        int [] game = {4,6,8, 9,2,1, 3,7,5}; // Hard test case
+        int [] game = {1,2,3, 9,5,6, 4,7,8}; // Easy test case
 
         SlidingGame s = new SlidingGame (game);
         System.out.println(s);
