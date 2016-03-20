fe0fa0bec6eec8292cb5f5eee0c751921fdad296
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-03-20 17:17:13 +0100

    Conversion functions

--- ObjectOrientatie/src/Exercise07/QTree.java
+++ ObjectOrientatie/src/Exercise07/QTree.java
@@ -1,10 +1,12 @@
 package Exercise07;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
 public class QTree {
     QTNode root;
+//    static String inputString;
     
     public QTree( Reader input ) {
         root = readQTree( input );
@@ -23,12 +25,51 @@ public class QTree {
     }
     
     private static QTNode readQTree( Reader input ) {
-        QTNode tempRoot = new WhiteLeaf();
+//        inputString = input.toString();
+        return recursiveQTreeFill (input);
+    }
+    
+    private static QTNode recursiveQTreeFill( Reader inputString ) {
+        QTNode currentNode;
         
-        return tempRoot;
+        int nodeLeafBit = '0';
+        try {
+            nodeLeafBit = inputString.read();
+        } catch (IOException ioe) {
+            System.out.println ("Provided string is not formatted correctly.\n Details: " + ioe);
+        }
+        if (nodeLeafBit == '0') { // Leaf
+            int colorBit = '0';
+            try {
+                colorBit = inputString.read();
+            } catch (IOException ioe) {
+                System.out.println ("Provided string is not formatted correctly.\n Details: " + ioe);
+            }
+            if (colorBit == '0') {
+                currentNode = new BlackLeaf ();
+            }
+            else {
+                currentNode = new WhiteLeaf ();
+            }
+        }
+        else { // Grey Node
+            QTNode [] children = new QTNode [4];
+            for (int i = 0; i < 4; i ++) {
+                children [i] = recursiveQTreeFill( inputString );
+            }
+            currentNode = new GreyNode (children);
+        }
+        
+        return currentNode;
     }
     
     public static QTNode bitmap2QTree( int x, int y, int width, Bitmap bitmap ) {
+        int depth = 1;
+        while(width > 1) {
+            width = width / 2;
+            depth ++;
+        }
+        
         return null;
     }
 
