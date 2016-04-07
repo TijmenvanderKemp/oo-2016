3f6fc394ae78e49f9a0c34b47a85f925318e2806
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-04-07 19:08:19 +0200

    Benodigde structuur aangebracht

---
+++ ObjectOrientatie/src/Exercise08/Opdracht1/IJsje.java
@@ -0,0 +1,15 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise08.Opdracht1;
+
+/**
+ *
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+public class IJsje {
+
+}
