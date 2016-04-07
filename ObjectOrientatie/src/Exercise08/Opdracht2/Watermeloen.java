29a8615490f5bf67b0fa731f53fbc6ab0a3e2a6b
veldhojo <jpj.v@hotmail.com>
2016-04-07 19:53:14 +0200

    artikelen en zo

---
+++ ObjectOrientatie/src/Exercise08/Opdracht2/Watermeloen.java
@@ -0,0 +1,12 @@
+ Error reading included file Templates/Classes/Templates/Licenses/license-object-orientation.txt
+package Exercise08.Opdracht2;
+
+/**
+ *
+ * @author Joep
+ */
+
+
+public class Watermeloen {
+    
+}
