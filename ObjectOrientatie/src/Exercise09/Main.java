77ed167be4f7c64f0053f81297dbb41c0461d2fc
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-04-16 23:27:07 +0200

    made a main

---
+++ ObjectOrientatie/src/Exercise09/Main.java
@@ -0,0 +1,20 @@
+/*
+ *  Made by Joep Veldhoven and Tijmen van der Kemp for the Object Oriëntatie class of 2016.
+ *  Do not copy or use without permission.
+ */
+
+package Exercise09;
+
+/**
+ * 
+ * @author Joep Veldhoven (s4456556)
+ * @author Tijmen van der Kemp (s4446887)
+ */
+
+public class Main {
+    
+    public static void main ( String [] args ) {
+        
+    }
+    
+}
