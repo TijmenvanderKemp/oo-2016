c3c558312671985efe4bb923c56029df039a8544
veldhojo <jpj.v@hotmail.com>
2016-05-04 20:37:57 +0200

    drop down list

--- ObjectOrientatie/src/Exercise11/View.java
+++ ObjectOrientatie/src/Exercise11/View.java
@@ -5,12 +5,15 @@
 
 package Exercise11;
 
+import java.util.LinkedList;
 import java.util.List;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -25,10 +28,13 @@ import javafx.stage.Stage;
 public class View extends Application {
     
     Controller controller;
+    List<String> gegevens = new LinkedList<>();
     
     @Override
     public void start(Stage stage)	{	
         controller = new Controller();
+        for(int i = 0; i < 6; i++)
+            gegevens.add("");
         
         GridPane grid =	new GridPane();	
 	grid.setAlignment(Pos.CENTER);	
@@ -48,14 +54,26 @@ public class View extends Application {
         
 	grid.add(new Label("Station:"),0,2);
         List<String> weerstations = controller.getWeerStations();
+        ChoiceBox<String> choice = new ChoiceBox<>();	
+	choice.getItems().addAll(weerstations);	
+	choice.setValue(weerstations.get(0));
+        grid.add(choice, 1, 2);
         // Implement dropdown
         
 	Button stationButton = new Button();	
-	stationButton.setText("ok");	
-	stationButton.setOnAction(e->{	
-		controller.getInfoAboutStation("Arnhem"); // CHANGE THIS TO RELEVANT THING
-        });
+	stationButton.setText("Vraag gegevens op");	
+	stationButton.setOnAction(new StationHandler());
 	grid.add(stationButton, 1, 3);	
+        
+        grid.add(new Label("Datum:"), 0, 4);
+        grid.add(new Label("Luchtvochtigheid:"), 0, 5);
+        grid.add(new Label("Temperatuur (C):"), 0, 6);
+        grid.add(new Label("Windsnelheid (bf):"), 0, 7);
+        grid.add(new Label("Windrichting:"),0 , 8);
+        grid.add(new Label("Regen (mm/h):"),0 , 9);
+        
+        grid.add(new Label(gegevens.get(2)), 1, 4);
+        
         
 	Scene scene = new Scene(grid, 300, 200);	
 	stage.setTitle("Buienradar.nl");	
@@ -64,8 +82,16 @@ public class View extends Application {
         
     }
     
+    private class StationHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent event) {
+            
+        }
+    }
+    
     public static void main (String [] args) {
         launch(args);
     }
     
+   
 }
