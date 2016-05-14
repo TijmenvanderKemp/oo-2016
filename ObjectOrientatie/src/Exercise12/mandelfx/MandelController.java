1ff62aae1611f6ba27fdd157c8729fd6ea782734
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-05-14 18:08:20 +0200

    basic layout complete

---
+++ ObjectOrientatie/src/Exercise12/mandelfx/MandelController.java
@@ -0,0 +1,25 @@
+/*
+ * Created by Joep Veldhoven and Tijmen van der Kemp for the course Object Orientation 2016
+ * Joep Veldhoven - s4456556
+ * Tijmen van der Kemp - s4446887
+ */
+package Exercise12.mandelfx;
+
+/**
+ *
+ * @author tijmen
+ */
+public class MandelController {
+    
+    private final MandelFX parentFX;
+    
+    public MandelController (MandelFX parent) {
+        parentFX = parent;
+        
+        
+    }
+    
+    public void dataEntered (String [] data) {
+        
+    }
+}
--- ObjectOrientatie/src/Exercise12/mandelfx/MandelFX.java
+++ ObjectOrientatie/src/Exercise12/mandelfx/MandelFX.java
@@ -11,6 +11,14 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import Exercise12.mandelmodel.AreaFiller;
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -20,9 +28,11 @@ public class MandelFX extends Application {
 
     public static final int GRID_WIDTH = 800, GRID_HEIGHT = 800;    
     private Canvas canvas;
+    private MandelController mandelController;
     
     @Override
     public void start(Stage primaryStage) {
+        mandelController = new MandelController(this);
         primaryStage.setTitle("Mandelbrot");
         primaryStage.setScene( makeScene() );
         primaryStage.show();
@@ -35,6 +45,39 @@ public class MandelFX extends Application {
         areaFiller.fill( canvas );
 
         Group root = new Group( canvas );
+        
+        GridPane g = new GridPane();
+        
+        g.add(new Label("Center (x, y):"), 0, 0);
+        g.add(new Label("Scale:"), 0, 1);
+        g.add(new Label("Repetitions:"), 0, 2);
+        
+        TextField centerXTextField = new TextField("0");
+        TextField centerYTextField = new TextField("0");
+        TextField scaleTextField = new TextField("100");
+        TextField repetitionsTextField = new TextField("20");
+        Button enterDataButton = new Button("Draw the fractal!");
+        
+        g.add(centerXTextField, 1, 0);
+        g.add(centerYTextField, 2, 0);
+        g.add(scaleTextField, 1, 1, 2, 1);
+        g.add(repetitionsTextField, 1, 2, 2, 1);
+        g.add(enterDataButton, 0, 3, 3, 1);
+        
+        root.getChildren().add(g);
+        
+        enterDataButton.setOnAction((ActionEvent e) -> 
+            mandelController.dataEntered(new String[]{
+                centerXTextField.getText(),
+                centerYTextField.getText(),
+                scaleTextField.getText(),
+                repetitionsTextField.getText()
+            })
+        );
+        
+        // Fire the button to draw the fractal for the first time
+        enterDataButton.fire();
+        
         Scene scene = new Scene(root);
         return scene;
     }
